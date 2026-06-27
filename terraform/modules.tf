66a2e50988105574132ac40788aa71bdd6f2cef0
jojo <elokajosephine10@gmail.com>
2026-06-27 12:08:53 -0700

    Use GitHub Terraform module source

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,6 +1,6 @@
 module "networking" {
 
-  source = "../../terraform-modules/networking"
+  source = "git::https://github.com/jojoaws/terraform-modules.git//networking"
 
   project_name = var.project_name
 }
